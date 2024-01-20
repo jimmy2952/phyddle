b5d749a52e2f8b5403e11b4d7d0a2e32103b8353
Jimmy Chen <jimmy2952@gmail.com>
2024-01-20 11:33:20 +0800

    feat: handle exist username and email

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,12 +1,31 @@
-// import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client"
+import { BadRequestError } from "@/errors/bad-request-error"
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 
 export async function POST(request: Request) {
   const res = await request.json()
+  const { username, email } = res
   console.log(res)
 
-  // const data = await res.json()
+  const isUsernameExist = await prisma.user.findUnique({
+    where: {
+      username
+    },
+  })
+
+  const isEmailExist = await prisma.user.findUnique({
+    where: {
+      email
+    },
+  })
+
+  if (isEmailExist || isUsernameExist) {
+    let existFields = []
+    if (isUsernameExist) existFields.push("username")
+    if (isEmailExist) existFields.push("email")
+    throw new BadRequestError(existFields.join(", ") + "had been used")
+  }
 
   return Response.json(res)
 }
