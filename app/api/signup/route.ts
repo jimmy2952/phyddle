2b0c1be2b4fc8481e214a4b37c0aa52187d60d4b
Jimmy Chen <jimmy2952@gmail.com>
2024-01-22 18:45:49 +0800

    feat(app/api/signup/route.ts): store user data for new user

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,12 +1,13 @@
 import { PrismaClient } from "@prisma/client"
 import { BadRequestError } from "@/errors/bad-request-error"
+import { Password } from "@/helpers/password"
+import { generateEmailVerificationToken } from "@/helpers/email_verification_token_generator"
 
 const prisma = new PrismaClient()
 
 export async function POST(request: Request) {
   const res = await request.json()
-  const { username, email } = res
-  console.log(res)
+  const { username, email, password } = res
 
   const isUsernameExist = await prisma.user.findUnique({
     where: {
@@ -25,6 +26,39 @@ export async function POST(request: Request) {
     if (isUsernameExist) existFields.push("username")
     if (isEmailExist) existFields.push("email")
     throw new BadRequestError(existFields.join(", ") + "had been used")
+  }
+
+  const hashedPassword = Password.getHashedPassword(password)
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      username,
+      password: hashedPassword
+    },
+  })
+
+  const account = await prisma.account.create({
+    data: {
+      userId: user.id,
+      type: "email",
+      provider: "phyddle",
+      providerAccountId: user.id,
+    }
+  })
+
+  if (user && account) {
+    // create verification token
+    const token = await generateEmailVerificationToken()
+    // expires after 2 hours
+    const expiredTime = new Date(new Date().getTime() + 60 * 60 * 1000 * 2)
+    await prisma.verificationToken.create({
+      data: {
+        identifier: user.email,
+        token,
+        expires: expiredTime,
+      }
+    })
   }
 
   return Response.json(res)
